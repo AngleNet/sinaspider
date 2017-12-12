aaa5f87ec8fae81211404a1014aaa5fd4c65c27b
AngleNet <anglenet@sina.cn>
2017-12-12 20:17:59 +0800

    Add downloader startup procedure.

--- sinaspider/downloader.py
+++ sinaspider/downloader.py
@@ -23,7 +23,7 @@ class Downloader(threading.Thread):
     call the finish callback. The downloader should do any redirects if needed.
     """
 
-    def __init__(self, name, pipeline):
+    def __init__(self, name):
         """
         Input:
         - name: A string of downloader name.
@@ -34,15 +34,28 @@ class Downloader(threading.Thread):
         self.user_identity = None           # user name and password
         self.loginer = SinaSessionLoginer(
             self.session)  # Login when session expired
-        self.logger = None
-        self.pipeline = pipeline
-
+        self._stop_event = threading.Event() 
     def run(self):
+        logger = logging.getLogger(self.name)
+        logger.info('Starting %s' % self.name)
+        transport = TSocket.TSocket(SCHEDULER_CONFIG['addr'],
+                                    SCHEDULER_CONFIG['port'])
+        transport = TTransport.TBufferedTransport(
+            transport)
+        protocol = TBinaryProtocol.TBinaryProtocol(
+            transport)
+        client = Client(protocol)
+        while not self._stop_event.is_set():
+            import time; time.sleep(2)
+            logger.info('pass')
+        logger.info('Downloader %s stopped' % self.name)
+
+    def _run(self):
         """
         Entry of the downloader. The main working loop.
         """
-        self.logger = logging.getLogger(self.name)
-        self.logger.info('Starting %s' % self.name)
+        logger = logging.getLogger(self.name)
+        logger.info('Starting %s' % self.name)
         transport = TSocket.TSocket(SCHEDULER_CONFIG['addr'],
                                     SCHEDULER_CONFIG['port'])
         transport = TTransport.TBufferedTransport(
@@ -52,40 +65,40 @@ class Downloader(threading.Thread):
         client = Client(protocol)
         while True:
             try:
-                self.logger.debug('Connecting scheduler_service %s' %
+                logger.debug('Connecting scheduler_service %s' %
                               SCHEDULER_CONFIG['addr'])
                 transport.open()
-                self.logger.debug('Connected.')
+                logger.debug('Connected.')
                 client.register_downloader(self.name)
-                self.logger.debug('Registered')
+                logger.debug('Registered')
                 self.user_identity = client.request_user_identity()
-                self.logger.debug('Get user identity: %s:%s' % \
+                logger.debug('Get user identity: %s:%s' % \
                         (self.user_identity.name, self.user_identity.pwd))
                 break
             except TTransport.TTransportException:
-                self.logger.exception('Exception while initializing %s, reconecting...' % self.name)
+                logger.exception('Exception while initializing %s, reconecting...' % self.name)
         while True:
             try:
                 if not transport.isOpen():
                     transport.open()
                 self.links = client.grab_links(
                     DOWNLOADER_CONFIG['link_batch_size'])
-                self.logger.debug('Grab links: %s' % self.links)
+                logger.debug('Grab links: %s' % self.links)
                 for _ in range(len(self.links)):
                     link = self.links[-1]
-                    self.logger.debug('Downloading %s.' % link)
+                    logger.debug('Downloading %s.' % link)
                     response = self._download(link)
                     if not response:
                         break
-                    self.pipeline.feed(response)
+                    import time; time.sleep(4)
                     del self.links[-1]
             except TTransport.TTransportException:
-                self.logger.exception('Exception in downloading loop: ')
+                logger.exception('Exception in downloading loop: ')
         if transport.isOpen():
             client.submit_links(self.links)
             client.unregister_downloader(self.name)
             transport.close()
-        self.logger.info('Downloader %s stopped.' % self.name)
+        logger.info('Downloader %s stopped.' % self.name)
 
     def _download(self, link):
         """
@@ -94,15 +107,16 @@ class Downloader(threading.Thread):
         Input:
         - link: A string of link to be downloaded.
         """
-        while True:
+        logger = logging.getLogger(self.name)
+        while not self._stop_event.is_set():
             try:
                 response = self.session.get(link)
                 if self._is_login(response):
                     return response
-                self.logger.info('Session expired. Relogin...')
+                logger.info('Session expired. Relogin...')
                 self.loginer.login(self.user_identity)
             except requests.RequestException:
-                self.logger.exception('Exception in downloading %s' % link)
+                logger.exception('Exception in downloading %s' % link)
         return None
 
     def _is_login(self, response):
@@ -110,4 +124,7 @@ class Downloader(threading.Thread):
         Should be implemented inline.
         """
         return True
+
+    def stop(self):
+        self._stop_event.set() 
 
--- start.py
+++ start.py
@@ -18,36 +18,35 @@ import sinaspider.weibo_pipeline
 class SinaSpiderDaemon(sinaspider.utils.Daemon):
     def __init__(self, pid_file):
         sinaspider.utils.Daemon.__init__(self, pid_file, self.__class__.__name__)
-        self.manager = multiprocessing.Manager()
+        self.manager = None
+        self.downloaders = list()
 
     def run(self):
         signal.signal(signal.SIGINT, self.exit_gracefully)
         signal.signal(signal.SIGTERM, self.exit_gracefully)
+        # TODO: Replace os.fork in Daemon to enable the creation of manager 
+        self.manager = multiprocessing.Manager()
         sinaspider.log.configure_logger('.downloader.log')
-        queue = self.manager.Queue(-1)
-        pipeline = sinaspider.weibo_pipeline.SpiderPipeline(queue)
-        engine = sinaspider.pipeline.PipelineEngine(pipeline, self.manager)
-        self.engine_server = multiprocessing.Process(target=engine.run)
-        self.engine_server.start()
-
-        downloaders = list()
+        logger = logging.getLogger(self.name)
         for idx in range(sinaspider.config.DOWNLOADER_CONFIG['num_downloaders']):
             name = sinaspider.config.DOWNLOADER_CONFIG['name_prefix'] + '-' + str(idx)
-            downloader = sinaspider.downloader.Downloader(name, pipeline)
-            downloaders.append(downloader)
+            logger.debug('Creating %s' % name)
+            downloader = sinaspider.downloader.Downloader(name)
+            self.downloaders.append(downloader)
         
-        for downloader in downloaders:
+        for downloader in self.downloaders:
+            logger.debug('Starting %s thread' % downloader.name)
             downloader.start()
         
-        for downloader in downloaders:
+        for downloader in self.downloaders:
+            logger.debug('Waiting %s finish' % downloader.name)
             downloader.join()
     
     def exit_gracefully(self, sig, func):
-        logger = logging.getLogger()
-        import os; os.kill(self.engine_server.pid, signal.SIGTERM)
-        logger.info('Waiting pipeline engine stopped.')
-        self.engine_server.join()
-        logger.info('Pipeline engine stopped')
+        logger = logging.getLogger(self.name)
+        for downloader in self.downloaders:
+            downloader.stop()
+        logger.debug('Exiting')
 
 if __name__ == '__main__':
     pid_dir = dirname(abspath(__file__))
